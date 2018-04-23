cb4408d1ca86149154ad5bb2a02419ee7a39b8ed
lidianvilazio <lidianvilazio@hotmail.com>
2018-04-23 15:05:05 -0400

    Done.

---
+++ src/components/FillerText.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Webpage from 'Webpage.js'
+
+
+export default class FillerText extends React.Component {
+  render() {
+    return (
+        <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
+    )
+  }
+
+}
